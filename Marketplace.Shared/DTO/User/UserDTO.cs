35e1a974e5e0646adda99a2e9e4bd5cefc179d55
hotl1ne <m.kurychja@gmail.com>
2025-02-21 12:44:47 +0200

    added `shared` project for commom files

---
+++ Marketplace.Shared/DTO/User/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Shared.DTO.User
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
