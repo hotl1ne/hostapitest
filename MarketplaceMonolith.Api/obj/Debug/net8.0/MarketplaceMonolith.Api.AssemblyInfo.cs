d10ed9915e5039e29abe85d5a9eec823f7e03b5a
hotl1ne <m.kurychja@gmail.com>
2025-03-06 13:11:27 +0200

    updated Dockerfile v7 and changed jwt package version

--- MarketplaceMonolith.Api/obj/Debug/net8.0/MarketplaceMonolith.Api.AssemblyInfo.cs
+++ MarketplaceMonolith.Api/obj/Debug/net8.0/MarketplaceMonolith.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketplaceMonolith.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26cf1fa1332fd9698b246bd6ff2dc2c74554680")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03073204324d3d31d2535401c0b027de2a101bf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketplaceMonolith.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketplaceMonolith.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
